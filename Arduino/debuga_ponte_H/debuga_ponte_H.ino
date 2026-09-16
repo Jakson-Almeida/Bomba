bcf71f2a3fd6a9c283dc0659c295950bbec0a802
Jakson Almeida <jakson.almeida@estudante.ufjf.br>
2026-09-16 19:29:40 -0300

    3 motores
    Portas novas

--- Arduino/debuga_ponte_H/debuga_ponte_H.ino
+++ Arduino/debuga_ponte_H/debuga_ponte_H.ino
@@ -18,6 +18,7 @@ struct Motor {
 // Canal A e canal B da TB6612FNG. Ajuste os GPIOs conforme a fiação.
 Motor M4 = {15, 2, 4};     // PWMA, AIN1, AIN2
 Motor M5 = {19, 18, 5};   // PWMB, BIN1, BIN2
+Motor M6 = {21, 3, 1};   // PWMB, BIN1, BIN2
 
 void setDirection(const Motor& motor, bool forward) {
   digitalWrite(motor.in1, forward ? HIGH : LOW);
@@ -51,6 +52,7 @@ void setup() {
 
   setupMotor(M4);
   setupMotor(M5);
+  setupMotor(M6);
 
   Serial.println("TB6612FNG pronta. Motores em sentido direto.");
   delay(500);
@@ -59,30 +61,36 @@ void setup() {
 void loop() {
   setDirection(M4, true);
   setDirection(M5, true);
+  setDirection(M6, true);
   for (int duty = 0; duty <= PWM_MAX; duty++) {
     setSpeed(M4, duty);
     setSpeed(M5, duty);
+    setSpeed(M6, duty);
     delay(10);
   }
 
   for (int duty = PWM_MAX; duty >= 0; duty--) {
     setSpeed(M4, duty);
     setSpeed(M5, duty);
+    setSpeed(M6, duty);
     delay(10);
   }
 
   setDirection(M4, false);
   setDirection(M5, false);
+  setDirection(M6, false);
 
   for (int duty = 0; duty <= PWM_MAX; duty++) {
     setSpeed(M4, duty);
     setSpeed(M5, duty);
+    setSpeed(M6, duty);
     delay(10);
   }
 
   for (int duty = PWM_MAX; duty >= 0; duty--) {
     setSpeed(M4, duty);
     setSpeed(M5, duty);
+    setSpeed(M6, duty);
     delay(10);
   }
 }
